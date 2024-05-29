f1430d68ea1d45ebf83683478ec6f4ea73f79073
Richard McCormick <mccormickri38@gmail.com>
2024-05-29 13:20:19 -0400

    Add script to display books on DOM

--- script.js
+++ script.js
@@ -2,13 +2,49 @@ console.log("ITS WORKING");
 
 const myLibrary = [];
 
-function Book(title, author, pages)  {
+function Book(title, author, pages, isRead)  {
     this.title = title;
     this.author = author;
     this.pages = pages;
+    this.isRead = isRead;
 }
 
-function addBookToLibrary(title, author, pages) {
-    myLibrary.push(new Book(title, author, pages));
+function addBookToLibrary(title, author, pages, isRead) {
+    myLibrary.push(new Book(title, author, pages, isRead));
 }
 
+//book display window
+const bookshelf = document.querySelector("#bookshelf")
+
+function createBookElement (book) {
+    const bookElement = document.createElement("div");
+    bookElement.classList.add("book");
+    const close = document.createElement("button");
+    close.classList.add("close");
+    close.textContent = "x";
+    const bookTitle = document.createElement("div");
+    bookTitle.classList.add("book-title");
+    bookTitle.textContent = book.title;
+    const readToggle = document.createElement("button");
+
+    if(book.isRead === true){
+        readToggle.classList.add("read");
+        readToggle.textContent = "Read";
+    }else {
+        readToggle.classList.add("unread");
+        readToggle.textContent = "Unread";
+    }
+
+    bookElement.appendChild(close);
+    bookElement.appendChild(bookTitle);
+    bookElement.appendChild(readToggle);
+
+    bookshelf.appendChild(bookElement);
+}
+
+function displayLibrary() {
+    //empties div
+    bookshelf.textContent = "";
+
+    myLibrary.forEach((book) => createBookElement(book));
+}
